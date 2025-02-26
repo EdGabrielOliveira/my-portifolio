d28931da7a936128c21c18f38e6d4cf3ed24967d
edgabrieloliveira <edgabrieloliveira@proton.me>
2025-02-26 10:07:12 -0300

    feat: Adicionar link para download do currículo digital no rodapé

--- src/app/components/Layout/Footer.tsx
+++ src/app/components/Layout/Footer.tsx
@@ -5,6 +5,13 @@ export default function Footer() {
     <div className="flex flex-col justify-center items-center md:pt-10 sm:pb-4">
       <p>Created by Gabriel Oliveira</p>
       <p className="text-xs">TypeScript, Tailwind CSS e Next.js</p>
+      <a
+        href="https://drive.google.com/file/d/1SY6NzyFi5MawCR1alkYCMe3Dpc0wGHFG/view?usp=sharing"
+        target="_blank"
+        className="mt-4 mb-4 p-2 px-6 bg-gradient-to-tr to-sky-700 from-sky-900 rounded-xl cursor-pointer hover:scale-95 hover:duration-200 transition-all"
+      >
+        Baixe o currículo digital
+      </a>
     </div>
   );
 }
